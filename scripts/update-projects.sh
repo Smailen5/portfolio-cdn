f2b7f78959020f0f6451f6cc262d3e1cf71f1fe2
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-09-26 16:52:45 +0200

    fix: 🐛 Corregge l'assegnazione del nome del progetto nello script di aggiornamento
    - Modificato il ciclo per estrarre correttamente il nome del progetto da `projects.nameFolder` a `project.nameFolder`.
    - Migliorata la leggibilità del codice con una corretta indentazione.

--- scripts/update-projects.sh
+++ scripts/update-projects.sh
@@ -6,12 +6,12 @@ node -e "
 const fs = require('fs');
 const projects = JSON.parse(fs.readFileSync('public/projects.json', 'utf8'));
 
-projects.forEach(project =>{
-// Estrae il nome del progetto dal file projects.json
-const projectName = projects.nameFolder;
+projects.forEach(project => {
+  // Estrae il nome del progetto dal file projects.json
+  const projectName = project.nameFolder;
 
-// Sovrascrive il link dell'immagine con il link della CDN
-project.imageUrl = `https://portfolio-cdn.netlify.app/images/previews/\${projectName}.webp\`;
+  // Sovrascrive il link dell'immagine con il link della CDN
+  project.imageUrl = \`https://portfolio-cdn.netlify.app/images/previews/\${projectName}.webp\`;
 });
 
 // Sovrascrive il file projects.json
