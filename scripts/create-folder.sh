16e3f060ec2f0e79a266b1c27e63c4f68919eb8c
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-09-26 14:13:54 +0200

    chore: 🗑️ Rimuove file .gitkeep e immagini non più necessarie
    - Eliminati i file `.gitkeep` dalle cartelle `full`, `previews` e `screenshots` per una pulizia del repository.
    - Rimosse diverse immagini screenshot non più necessarie, ottimizzando la gestione delle risorse visive nel progetto.

---
+++ scripts/create-folder.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "📁 Creando struttura cartelle..."
+
+# Crea la struttura delle cartelle se non esistono
+mkdir -p public
+mkdir -p public/images/previews
+mkdir -p public/images/full
+mkdir -p public/images/screenshots
+
+echo "✅ Struttura cartelle creata"
+
+# $ chmod +x scripts/create-folder.sh
